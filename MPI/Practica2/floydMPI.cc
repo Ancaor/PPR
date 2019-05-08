afb9ff45365eb09d6213e935f0a368393df82291
Antonio <Ancaor@correo.ugr.es>
2019-05-08 12:55:58 +0200

    make hecho

--- MPI/Practica2/floydMPI.cc
+++ MPI/Practica2/floydMPI.cc
@@ -8,175 +8,180 @@
 #include <cmath>
 
 using namespace std;
-  
-int main(int argc, char** argv) {
- 
-    int rank, size, nverts;
-    MPI_Comm COMM_VERTICAL, COMM_HORIZONTAL;
-    Graph grafo;
+
+//**************************************************************************
+
+int main (int argc, char *argv[])
+{
+
+    int rank, size;
     MPI_Datatype MPI_BLOQUE;
- 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
- 
-    //** Lectura del grafo **//
+    MPI_Comm COMM_HORIZONTAL, COMM_VERTICAL;
+
+    MPI_Init(&argc, &argv); // Inicializamos la comunicacion de los procesos
+    MPI_Comm_size(MPI_COMM_WORLD, &size); // Obtenemos el numero total de hebras
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Obtenemos el valor de nuestro identificador
 
 
-    if(rank == 0){
-        
-        grafo.lee(argv[1]); // Lee el grafo del archivo indicado
+    if (argc != 2) {
+        cerr << "LLamada correcta: mpirun -np <nº hebras> " << argv[0] << " <archivo de grafo>"
+        << endl;
+        return(-1);
+    }
 
-        nverts = grafo.vertices;
+
+    /****************************** Lectura del grafo *****************************/
+
+    int nverts;
+    Graph G;
+
+    if (rank == 0){
+        G.lee(argv[1]);      
+        nverts=G.vertices;
     }
 
     // Broadcast de nverts a todos los procesos
-
     MPI_Bcast(&nverts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+    const int dim_submatriz = nverts/sqrt(size), // Filas, columnas de las submatrices
+        bsize = dim_submatriz*dim_submatriz, // Tamaño de bloque
+        grid_dim= sqrt(size); // Dimension del grid
+                       
+
+    // Comunicadores horizontal y vertical
+    MPI_Comm_split(MPI_COMM_WORLD, rank/grid_dim, rank, &COMM_VERTICAL);
+    MPI_Comm_split(MPI_COMM_WORLD, rank%grid_dim, rank, &COMM_HORIZONTAL);
+
+    int i, j, vikj, pos, fila_P, columna_P, comienzo;
+
+
+    /****************************** Reparto de bloques *****************************/
+
+    int submatriz[dim_submatriz][dim_submatriz];
+    int *matriz_grafo = new int[nverts*nverts];
+
+    if (rank == 0){
+
+    int * matrizOriginal = G.getMatriz(); // Matriz original que representa el grafo
     
-    const int subFilCol = nverts/sqrt(size); // tamaño de submatriz
-    const int bsize = subFilCol * subFilCol;
-    const int dim = sqrt(size);
+    // Defino y creo el tipo bloque cuadrado
+    MPI_Type_vector(dim_submatriz,dim_submatriz,nverts,MPI_INT,&MPI_BLOQUE);
+    MPI_Type_commit (&MPI_BLOQUE);
 
-    // creo un comunicador vertical y otro horizontal
+    //Empaquetado del bloque
+    for (i=0, pos=0; i<size; i++) {
+        // Calculo la posicion de comienzo de cada submatriz
+        fila_P = i/grid_dim;
+        columna_P = i%grid_dim;
+        comienzo = columna_P * dim_submatriz + fila_P * bsize * grid_dim;
 
-    MPI_Comm_split(MPI_COMM_WORLD,rank/dim,rank,&COMM_VERTICAL);
-    MPI_Comm_split(MPI_COMM_WORLD,rank%dim,rank,&COMM_HORIZONTAL);
-    
-    //** Reparto de bloques **//
-
-    //matriz de recepcion
-    int *matrizRecepcion = new int[nverts*nverts];
-
-    //submatriz de cada bloque
-    int submatriz[subFilCol][subFilCol];
-    
-
-    if(rank == 0){
-
-        //matriz original
-        int *I = grafo.getMatriz();
-
-
-        //Creo el tipo de dato Bloque
-        MPI_Type_vector (subFilCol,subFilCol,nverts,MPI_INT,&MPI_BLOQUE);
-        MPI_Type_commit(&MPI_BLOQUE);
-
-        //Empaqueto los bloques 
-
-        for(int i=0, pos = 0; i < size; i++){
-            int filaP = i/dim; // fila del proceso i
-            int columnaP = i%dim; // columna del proceso i
-            int posComienzo = columnaP * subFilCol + filaP * bsize * dim;
-
-            MPI_Pack(I + posComienzo,1,MPI_BLOQUE,matrizRecepcion,sizeof(int)*nverts*nverts,&pos,MPI_COMM_WORLD);
-
-        }
+        MPI_Pack (matrizOriginal + comienzo,1,MPI_BLOQUE,matriz_grafo,sizeof(int)*nverts*nverts,&pos,MPI_COMM_WORLD);
     }
 
-    // Reparto de bloques 
+    }
 
-    MPI_Scatter(matrizRecepcion,sizeof(int)*bsize,MPI_PACKED,submatriz,bsize,MPI_INT,0,MPI_COMM_WORLD);
+    // Distribuimos la matriz entre los procesos
+    MPI_Scatter(matriz_grafo,sizeof(int)*bsize,MPI_PACKED,submatriz,bsize,MPI_INT,0,MPI_COMM_WORLD);
 
-    // Barrera de sincronizacion
+    // Sincronizamos los procesos para medir tiempos
     MPI_Barrier(MPI_COMM_WORLD);
 
-    double t1 = MPI_Wtime();
-
-    /** Algoritmo de Floyd **/
+    double t1=MPI_Wtime();
 
 
-    int fila_k[subFilCol];
-    int columna_k[subFilCol];
-    int i_global, j_global; // indices globales
-    int idProcesoK; // id del proceso que contiene la fila y columna k-esima
 
-    for(int k = 0; k < nverts; k++){
-        i_global = (rank/dim)*subFilCol;
-        j_global = (rank%dim)*subFilCol;
 
-        idProcesoK = k/subFilCol;
+    /****************************** Algoritmo de Floyd *****************************/
 
-        //compruebo si la fila y columna k estan en nuestra submatriz
-        bool tiene_fila_k    = k >= i_global && k < i_global + subFilCol;
-        bool tiene_columna_k = k >= j_global && k < j_global + subFilCol;
+    int idProcesoK;
+    int filaK[dim_submatriz]; 
+    int columnaK[dim_submatriz];
 
-        if(tiene_fila_k)
-            memcpy(fila_k, &submatriz[k%subFilCol][0],sizeof(int)*subFilCol);
+    // indices globales de filas y columnas
+    int i_global;
+    int j_global;
 
-        if(tiene_columna_k)
-            for(int i = 0; i < subFilCol; i++){
-                columna_k[i] = submatriz[i][k%subFilCol];
-            }
-    
+    for (int k=0; k < nverts; k++)
+    {
+        i_global = (rank/grid_dim)*dim_submatriz;
+        j_global = (rank%grid_dim)*dim_submatriz;
 
-        //Broadcast de la fila y columna k-esima
-        MPI_Bcast(fila_k,subFilCol,MPI_INT, idProcesoK,COMM_HORIZONTAL);
-        MPI_Bcast(columna_k,subFilCol,MPI_INT, idProcesoK,COMM_VERTICAL);
+        // proceso que tiene la fila y columnaK
+        idProcesoK = k/dim_submatriz;
 
-        i_global = (rank/dim)*subFilCol;
+        // Comprobamos si la fila y columna k-ésima están en nuestra submatriz
+        bool tienesFilaK = k >= i_global && k < i_global+dim_submatriz;
+        bool tienesColumnaK = k >= j_global && k < j_global+dim_submatriz;
 
-        for(int i = 0; i < subFilCol; i++, i_global++){
-            int j_global = (rank%dim)*subFilCol;
-            for(int j = 0; j < subFilCol; j++, j_global++){
-                if(i_global!=j_global && i_global!=k && j_global!=k){
-                    int vikj = min(fila_k[j]+columna_k[i],submatriz[i][j]);
-                    submatriz[i][j] = vikj;
+        if (tienesFilaK)
+            memcpy(filaK, &submatriz[k%dim_submatriz][0], sizeof(int)*dim_submatriz);
+
+        if (tienesColumnaK)
+            for (i = 0; i < dim_submatriz; i++)
+                columnaK[i] = submatriz[i][k%dim_submatriz];
+
+        // Broadcast de la fila y columna k-ésima
+        MPI_Bcast(filaK, dim_submatriz, MPI_INT, idProcesoK, COMM_HORIZONTAL);
+        MPI_Bcast(columnaK, dim_submatriz, MPI_INT, idProcesoK, COMM_VERTICAL);
+
+        i_global = (rank/grid_dim)*dim_submatriz;
+
+        for (i = 0; i < dim_submatriz; i++, i_global++){
+            j_global = (rank%grid_dim)*dim_submatriz;
+            for (j = 0; j < dim_submatriz; j++, j_global++)
+                if (i_global!=j_global && i_global!=k && j_global!=k){
+                    vikj=min(filaK[j]+columnaK[i], submatriz[i][j]);
+                    submatriz[i][j]=vikj;
                 }
-            }
         }
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        double t2 = MPI_Wtime();
-
-
-        /** Obtencion de resultados **/
-
-        //primero realizo un gather para obtener todos los resultados en el proceso 0
-
-        MPI_Gather(submatriz,bsize,MPI_INT,matrizRecepcion,sizeof(int)*bsize,MPI_PACKED,0,MPI_COMM_WORLD);
-
-        int * matriz_resultado = new int[nverts*nverts];
-
-        if(rank == 0){
-
-            // desempaqueto 
-
-            for(int i=0, pos = 0; i < size; i++){
-                int filaP = i/dim; // fila del proceso i
-                int columnaP = i%dim; // columna del proceso i
-                int posComienzo = columnaP * subFilCol + filaP * bsize * dim;
-
-                MPI_Unpack(matrizRecepcion,sizeof(int)*nverts*nverts,&pos,matriz_resultado+posComienzo,1,MPI_BLOQUE,MPI_COMM_WORLD);
-            }
-
-            //Libero el tipo de dato bloque
-            MPI_Type_free(&MPI_BLOQUE);
-
-             cout << endl<<"EL Grafo con las distancias de los caminos más cortos es:\n";
-            for(int i=0;i<nverts;i++){
-                cout << "A["<<i << ",*]= ";
-                for(int j=0;j<nverts;j++){
-                if (matriz_resultado[i*nverts+j]==INF) cout << "INF";
-                else cout << matriz_resultado[i*nverts+j];
-                if (j<nverts-1) cout << ",";
-                else cout << endl;
-                }
-            }
-
-            delete matriz_resultado;
-
-            cout <<endl <<"Tiempo de Floyd 2D con MPI : " << t2-t1 << endl;
-            
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
-        delete matrizRecepcion;
-        MPI_Finalize();
-
-        return 0;
     }
 
+    MPI_Barrier(MPI_COMM_WORLD);
 
+    double  t2=MPI_Wtime();
+    t2 = t2-t1;
+
+
+
+
+    /****************************** Obtencion de resultados *****************************/
+
+    MPI_Gather(submatriz,bsize,MPI_INT,matriz_grafo,sizeof(int)*bsize,MPI_PACKED,0,MPI_COMM_WORLD);
+
+    int * matrizResultado = new int[nverts*nverts];
+
+    if (rank == 0){
+
+    for (i=0, pos=0; i<size; i++) {
+        // Calculo la posicion de comienzo de cada submatriz de cada proceso
+        fila_P = i/grid_dim;
+        columna_P = i%grid_dim;
+        comienzo = columna_P * dim_submatriz + fila_P * bsize * grid_dim;
+
+        MPI_Unpack(matriz_grafo,sizeof(int)*nverts*nverts,&pos,matrizResultado + comienzo,1,MPI_BLOQUE,MPI_COMM_WORLD);
+    }
+
+    MPI_Type_free (&MPI_BLOQUE);
+
+    cout << "Tiempo MPI-2D: " << t2 << endl << endl;
+
+    cout << endl<<"EL Grafo con las distancias de los caminos más cortos es:\n";
+    for(i=0;i<nverts;i++){
+        cout << "A["<<i << ",*]= ";
+        for(j=0;j<nverts;j++){
+        if (matrizResultado[i*nverts+j]==INF) cout << "INF";
+        else cout << matrizResultado[i*nverts+j];
+        if (j<nverts-1) cout << ",";
+        else cout << endl;
+        }
+    }
+
+    delete matrizResultado;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    delete matriz_grafo;
+    MPI_Finalize();
+
+    return 0;
 }
